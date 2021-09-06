d43ffb53089033d4d18a9818b6d38c12184a47fb
sebastianfori <88661632+sebastianfori@users.noreply.github.com>
2021-09-06 10:34:43 -0300

    Add files via upload

---
+++ src/Recipe.cs
@@ -0,0 +1,52 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class Recipe
+    {
+        
+        private ArrayList steps = new ArrayList();
+
+        public Product FinalProduct { get; set; }
+
+        public void AddStep(Step step)
+        {
+            this.steps.Add(step);
+        }
+
+        public void RemoveStep(Step step)
+        {
+            this.steps.Remove(step);
+        }
+        public double GetProductionCost()
+        {
+            double costoInsumos = 0;
+            double costoEquipamiento = 0;
+
+            foreach (Step step in steps)
+            {
+                costoInsumos += step.Input.UnitCost*step.Quantity;
+                costoEquipamiento+= step.Time*step.Equipment.HourlyCost;
+
+            }
+            return costoInsumos+costoEquipamiento;
+        }
+
+        public void PrintRecipe()
+        {
+            Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
+            foreach (Step step in this.steps)
+            {
+                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+            }
+        }
+    }
+}
